99c7a798aa85638485d6fe8e012c2e3ccad6a527
Gustavo Barragan <dev@droni.co>
2022-09-13 20:54:30 -0500

    routes and front app

--- config/database.php
+++ config/database.php
@@ -7,14 +7,13 @@ $db = new Capsule;
 $db->addConnection([
   "driver" => "sqlsrv",
   "host" => $_ENV['DB_HOST'],
-  "database" => $_ENV['DB_DATABASE'],
-  "username" => $_ENV['DB_USERNAME'],
-  "password" => $_ENV['DB_PASSWORD']
+  "database" => $_ENV['DB_NAME'],
+  "username" => $_ENV['DB_USER'],
+  "password" => $_ENV['DB_PASS']
 ]);
 
 //Make this Capsule instance available globally.
 $db->setAsGlobal();
 
 // Setup the Eloquent ORM.
-$db->bootEloquent();
 $db->bootEloquent();
--- config/router.php
+++ config/router.php
@@ -2,11 +2,16 @@
 $router = new \Bramus\Router\Router();
 
 // Define routes
-$router->get('/', '\Ods\Migrator@index');
-$router->get('/show', '\Ods\Migrator@show');
-$router->get('/procedures', '\Ods\Migrator@procedures');
-$router->post('/export', '\Ods\Migrator@export');
-$router->post('/import', '\Ods\Migrator@import');
+$router->get('/', function() {
+  header('Location: /front_app/dist/');
+  exit;
+});
+$router->get('/api/install/migrate', '\Usuarium\InstallController@migrate');
+$router->get('/api/install/fetcher', '\Usuarium\InstallController@fetcher');
+$router->get('/api/users', '\Usuarium\UserController@index');
+$router->get('/api/users/{id}', '\Usuarium\UserController@show');
+$router->post('/api/users', '\Usuarium\UserController@store');
+$router->put('/api/users/{id}', '\Usuarium\UserController@update');
 
 // Run it!
 $router->run();
